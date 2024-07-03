1c0cbd53163f8908e4a2d900625027b5258da2e3
Igor Grabowski <dogodontexist@gmail.com>
2024-07-03 16:30:52 +0200

    add editor Route and change path /how to /rules
    /rules has more meaning in the context of rules of CV than /how

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import Home from "./pages/Home";
 import NoPageFound from "./pages/NoPageFound";
 import { Redirect } from "./ui/Redirect";
 import { Nav } from "./ui/Nav";
+import RulesCV from "./pages/RulesCV";
+import Editor from "./pages/Editor";
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Redirect />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/rules" element={<RulesCV />} />
+          <Route path="/editor" element={<Editor />} />
           <Route path="*" element={<NoPageFound />} />
         </Routes>
       </BrowserRouter>
