deb54dc1e7e74af6c33512012fc85c847773d5a8
Igor Grabowski <dogodontexist@gmail.com>
2024-07-03 16:31:54 +0200

    add Rule component
    made Rules page prettier

---
+++ src/ui/Rule.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const StyledRule = styled.div`
+  padding: 2rem 0 2rem 0;
+`;
+
+const RuleTitle = styled.p`
+  font-size: 1.6rem;
+  font-weight: bold;
+  margin: 0;
+`;
+
+const RuleDesc = styled.p`
+  font-size: 1.4rem;
+  font-weight: bold;
+  color: #575757;
+  margin: 0;
+`;
+
+type RuleProps = { title: string; desc: string; numStr: string };
+
+export const Rule = ({ numStr, title, desc }: RuleProps) => {
+  return (
+    <StyledRule>
+      <RuleTitle>
+        {numStr} - {title}.
+      </RuleTitle>
+      <RuleDesc>{desc}.</RuleDesc>
+    </StyledRule>
+  );
+};
