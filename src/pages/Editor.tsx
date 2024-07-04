483a10d8db58e326d78fffdd460ebfe85f02f66c
Igor Grabowski <dogodontexist@gmail.com>
2024-07-04 12:44:10 +0200

    start creating an editor

--- src/pages/Editor.tsx
+++ src/pages/Editor.tsx
@@ -1,5 +1,37 @@
+import styled from "styled-components";
+
+const Site = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10%;
+  height: 100dvh;
+  overflow-x: hidden;
+`;
+// Inputs
+
+const InputsArea = styled.div`
+  width: 20%;
+  height: 70%;
+  background-color: #ffffff;
+  border: 1px solid #1c1c1c;
+`;
+
+// CV
+const CV = styled.div`
+  width: 30%;
+  height: 80%;
+  background-color: #ffffff;
+  border: 1px solid #1c1c1c;
+`;
+
 const Editor = () => {
-  return <div>editor_placeholder</div>;
+  return (
+    <Site>
+      <InputsArea>a</InputsArea>
+      <CV>b</CV>
+    </Site>
+  );
 };
 
 export default Editor;
